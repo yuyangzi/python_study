4f2d6ef13eb64aa8829eae45154a34c7d9ba2e9a
wangyiming <wangyimig@icloud.com>
2018-06-21 23:18:14 +0800

    学习Python 函数用法

---
+++ src/function/function02.py
@@ -0,0 +1,9 @@
+def damage(skill1, skill2):
+    damage1 = skill1 + 2
+    danage2 = skill2 * 3 + 100
+
+    return damage1, danage2
+
+skill_damage1, skill_damage2 = damage(3, 7)
+
+print(skill_damage1, skill_damage2)
